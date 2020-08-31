4ab6bfea6fddbacf7e1f80ba4e9fe9fc6726cd8f
Yannick <20306025+yandeu@users.noreply.github.com>
2020-08-31 20:40:54 +0200

    Added theme-color and apple-touch-icon

--- src/server/makeHtml.tsx
+++ src/server/makeHtml.tsx
@@ -19,7 +19,9 @@ export const makeHtml = (
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="manifest" href="/manifest.webmanifest">   
+    <meta name="theme-color" content="#3367D6">
+    <link rel="manifest" href="/manifest.webmanifest">
+    <link rel="apple-touch-icon" href="/public/img/icons/icons-192.png">
     ${head.join('\n')}
   </head>
 
