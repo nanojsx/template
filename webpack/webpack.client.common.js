1ea0e78b38ed46520c552568105bf3ef935cc347
yandeu <20306025+yandeu@users.noreply.github.com>
2020-12-30 00:34:33 +0100

    Added filter to WebpackManifestPlugin

--- webpack/webpack.client.common.js
+++ webpack/webpack.client.common.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const glob = require('glob')
 const CopyPlugin = require('copy-webpack-plugin')
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 
 module.exports = {
   // create one bundle for each file in /src/client
@@ -30,6 +30,12 @@ module.exports = {
     new CopyPlugin({
       patterns: [{ from: 'public', to: '../' }]
     }),
-    new WebpackManifestPlugin({ publicPath: '', basePath: '', })
+    new WebpackManifestPlugin({
+      publicPath: '',
+      basePath: '',
+      filter: file => {
+        return /\.hydrate\.js$/.test(file.name)
+      }
+    })
   ]
 }
