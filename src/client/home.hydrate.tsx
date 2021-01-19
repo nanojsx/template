f51b9a370b72b1c4c5504e4e362b55ca016adfd8
yandeu <20306025+yandeu@users.noreply.github.com>
2021-01-19 15:51:41 +0100

    Update to v0.0.14

--- src/client/home.hydrate.tsx
+++ src/client/home.hydrate.tsx
@@ -1,8 +1,12 @@
 import * as Nano from 'nano-jsx/lib/core'
+import { printVersion } from 'nano-jsx/lib/helpers'
 import TodoList from '../components/TodoList'
 
 const hydrate = async () => {
   Nano.hydrate(<TodoList />, document.getElementById('todo-list'))
+
+  // print the nano-jsx version in the console
+  printVersion()
 }
 
 hydrate()
